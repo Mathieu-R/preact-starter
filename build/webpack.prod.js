6e5b85b772ebbc1d9296468c84f0dbd26afe48db
Mathieu-R <mathieu0709@gmail.com>
2016-08-25 22:58:15 +0200

    init

--- build/webpack.prod.js
+++ build/webpack.prod.js
@@ -12,4 +12,4 @@ config.plugins = config.plugins.concat([
 ])
 
 // On extrait le CSS
-config.module.loaders[0].loaders = new ExtractTextPlugin.extract(config.module.loaders[0].loaders)
+config.module.loaders[0].loaders = ExtractTextPlugin.extract(config.module.loaders[0].loaders)
