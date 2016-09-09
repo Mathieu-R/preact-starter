c0a51858ebd1b09d51df07aad4d865f4e96b4efa
Mathieu-R <mathieu0709@gmail.com>
2016-09-09 09:55:09 +0200

    fix

--- build/webpack.base.js
+++ build/webpack.base.js
@@ -9,7 +9,7 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, '../dist'),
-    filename: 'bundle.js',
+    filename: '[name].js',
     publicPath: '/dist'
   },
   resolve: {
