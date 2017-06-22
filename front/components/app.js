05f1dd3a9323932f2ba9fafe86afefcda1701a23
Mathieu-R <mathieu0709@gmail.com>
2017-06-22 21:56:11 +0200

    rework folders

---
+++ front/components/app.js
@@ -0,0 +1,14 @@
+import { h, render, Component } from 'preact';
+import 'src/styles/style';
+
+class App extends Component {
+  render() {
+    return (
+      <section class="content">
+        <h1>Hello World !</h1>
+      </section>
+    );
+  }
+}
+
+export default App;
